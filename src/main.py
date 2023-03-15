cd82c6d5f42770f309cc0e602224e307db2e2683
kradja <kevin.radja@gmail.com>
2023-03-14 23:04:35 -0400

    starting

---
+++ src/main.py
@@ -0,0 +1,27 @@
+import argparse
+import os
+
+import data_preprocessing as dp
+
+# https://github.com/fouticus/pipgcn/tree/fd5f12c26fcabf4934d0a27fbc5a6753e0910fcd
+# https://zenodo.org/record/1127774#.ZBD7fy-B0UR
+
+
+def main(args):
+    params = {"raw": "../data/00_raw_data/", "processed": "../data/01_processed/"}
+    if args.preprocess:
+        train = dp.convert_data(os.path.join(params["raw"], "train.cpkl"))
+        test = dp.convert_data(os.path.join(params["raw"], "test.cpkl"))
+        dp.write_parquet(train, os.path.join(params["processed"], "train.parquet"))
+        dp.write_parquet(test, os.path.join(params["processed"], "test.parquet"))
+        print("convert")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-preprocess",
+        action="store_true",
+    )
+    args = parser.parse_args()
+    main(args)
