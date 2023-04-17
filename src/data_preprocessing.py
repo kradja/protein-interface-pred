b1c0f94decdf636560dd1187e90df7e8d5458997
kradja <kevin.radja@gmail.com>
2023-04-17 12:59:47 -0400

    adding preprocessing and writing to  parquet

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,7 +1,10 @@
+import itertools
 import os
 import pdb
 import pickle
+import random
 
+import numpy as np
 import pandas as pd
 import polars as pl
 
@@ -12,7 +15,42 @@ def convert_data(input_file):
     return data
 
 
-def write_parquet(data, output_file):
-    pdb.set_trace()
-    # rr = [{"r_vertex":x['r_vertex'],"l_vertex":x["l_vertex"],"label":x["label"]} for x in data[1]]
-    print("End")
+def write_parquet(data, output_file,neg_ratio=50):
+    """There is a 70 length vector of features for each amino acid within a protein structure"""
+    features_label = []
+    for ind, complex_ind in enumerate(data[0]):
+        ligand_aa_features = data[1][ind]["l_vertex"]
+        receptor_aa_features = data[1][ind]["r_vertex"]
+        aa_combinations = set(
+            itertools.product(
+                range(np.shape(ligand_aa_features)[0]),
+                range(np.shape(receptor_aa_features)[0]),
+            )
+        )
+        aa_label = data[1][ind]["label"]
+        pdb.set_trace()
+        mask = aa_label[:, 2] == 1
+        aa_truth_label = aa_label[mask, :]
+        aa_truth_label = set(map(tuple, aa_truth_label[:, (0, 1)]))
+        aa_neg_label = random.sample(
+            list(aa_combinations - aa_truth_label), len(aa_truth_label) * neg_ratio
+        )
+        features_label.extend(
+            [
+                np.concatenate(
+                    (ligand_aa_features[x[0]], receptor_aa_features[x[1]], 1),
+                    axis=None,
+                )
+                for x in aa_truth_label
+            ]
+        )
+        features_label.extend(
+            [
+                np.concatenate(
+                    (ligand_aa_features[x[0]], receptor_aa_features[x[1]], 0), axis=None
+                )
+                for x in aa_neg_label
+            ]
+        )
+    df = pd.DataFrame(features_label, dtype="float32[pyarrow]")
+    df.to_parquet(output_file, compression="gzip", index=False)
--- src/main.py
+++ src/main.py
@@ -10,18 +10,26 @@ import data_preprocessing as dp
 
 def main(args):
     params = {"raw": "../data/00_raw_data/", "processed": "../data/01_processed/"}
+    train_file= os.path.join(params["processed"], "train.parquet.gzip")
+    test_file= os.path.join(params["processed"], "test.parquet.gzip")
     if args.preprocess:
         train = dp.convert_data(os.path.join(params["raw"], "train.cpkl"))
         test = dp.convert_data(os.path.join(params["raw"], "test.cpkl"))
-        dp.write_parquet(train, os.path.join(params["processed"], "train.parquet"))
-        dp.write_parquet(test, os.path.join(params["processed"], "test.parquet"))
+        dp.write_parquet(train, train_file)
+        dp.write_parquet(test, test_file)
         print("convert")
+    if args.testing:
+        print("hey")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-preprocess",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-testing",
         action="store_true",
     )
     args = parser.parse_args()
