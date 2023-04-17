5a59437c66b8a83353ed5a89f9981ffa74de0f8f
kradja <kevin.radja@gmail.com>
2023-04-17 16:38:38 -0400

    edge_features and edges

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -20,37 +20,50 @@ def write_parquet(data, output_file,neg_ratio=50):
     features_label = []
     for ind, complex_ind in enumerate(data[0]):
         ligand_aa_features = data[1][ind]["l_vertex"]
+        length_ligand = len(ligand_aa_features)
         receptor_aa_features = data[1][ind]["r_vertex"]
-        aa_combinations = set(
-            itertools.product(
-                range(np.shape(ligand_aa_features)[0]),
-                range(np.shape(receptor_aa_features)[0]),
-            )
-        )
+        length_receptor= len(receptor_aa_features)
+        #aa_combinations = set(
+        #    itertools.product(
+        #        range(np.shape(ligand_aa_features)[0]),
+        #        range(np.shape(receptor_aa_features)[0]),
+        #    )
+        #)
         aa_label = data[1][ind]["label"]
+        ligand_ind = data[1][ind]['l_hood_indices']
+        ligand_edges = data[1][ind]['l_edge']
+        receptor_ind = data[1][ind]['r_hood_indices']
+
+        # Adjacency matrices
+        ligand_adj = np.zeros((length_ligand,length_ligand), int)
+        for l_ind,x in enumerate(ligand_ind):
+            ligand_adj[l_ind,x] = 1
+        receptor_adj = np.zeros((length_receptor,length_receptor), int)
+        for r_ind,x in enumerate(receptor_ind):
+            receptor_adj[r_ind,x] = 1
         pdb.set_trace()
-        mask = aa_label[:, 2] == 1
-        aa_truth_label = aa_label[mask, :]
-        aa_truth_label = set(map(tuple, aa_truth_label[:, (0, 1)]))
-        aa_neg_label = random.sample(
-            list(aa_combinations - aa_truth_label), len(aa_truth_label) * neg_ratio
-        )
+        #mask = aa_label[:, 2] == 1
+        #aa_truth_label = aa_label[mask, :]
+        #aa_truth_label = set(map(tuple, aa_truth_label[:, (0, 1)]))
+        #aa_neg_label = random.sample(
+        #    list(aa_combinations - aa_truth_label), len(aa_truth_label) * neg_ratio
+        #)
         features_label.extend(
             [
                 np.concatenate(
-                    (ligand_aa_features[x[0]], receptor_aa_features[x[1]], 1),
+                    (ligand_aa_features[x[0]], receptor_aa_features[x[1]], x[2]),
                     axis=None,
                 )
-                for x in aa_truth_label
+                for x in aa_label
             ]
         )
-        features_label.extend(
-            [
-                np.concatenate(
-                    (ligand_aa_features[x[0]], receptor_aa_features[x[1]], 0), axis=None
-                )
-                for x in aa_neg_label
-            ]
-        )
+        #features_label.extend(
+        #    [
+        #        np.concatenate(
+        #            (ligand_aa_features[x[0]], receptor_aa_features[x[1]], 0), axis=None
+        #        )
+        #        for x in aa_neg_label
+        #    ]
+        #)
     df = pd.DataFrame(features_label, dtype="float32[pyarrow]")
     df.to_parquet(output_file, compression="gzip", index=False)
